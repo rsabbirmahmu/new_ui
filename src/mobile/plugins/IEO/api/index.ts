7ff9dacd36d80e4d00a28ce05a37490f3b3b41ff
rsabbirmahmu <99828818+rsabbirmahmu@users.noreply.github.com>
2022-02-17 00:43:27 +0600

    Update index.ts

--- src/mobile/plugins/IEO/api/index.ts
+++ src/mobile/plugins/IEO/api/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'https://api-circleex.herokuapp.com/',
+	baseURL: 'https://api-udonex.herokuapp.com/',
+	// baseURL: 'https://api-circleex.herokuapp.com/',
 	headers: {
 		'Access-Control-Allow-Origin': '*',
 		'Content-Type': 'application/json',
